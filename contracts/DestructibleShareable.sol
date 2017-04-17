762f38f56e475c6661b88384cf0cbe87a8dc115e
Xander Dunn <xander.dunn@icloud.com>
2017-04-17 00:24:40 -0700

    Make Numeraire.sol Destructible
    When we upgrade it we will want to destroy the old one

---
+++ contracts/DestructibleShareable.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.10;
+
+
+import "Shareable.sol";
+
+// From OpenZepplin: https://github.com/OpenZeppelin/zeppelin-solidity/blob/master/contracts/lifecycle/Destructible.sol
+/*
+ * Destructible
+ * Base contract that can be destroyed by owner. All funds in contract will be sent to the owner.
+ */
+contract DestructibleShareable is Shareable {
+  function DestructibleShareable(address[] _owners, uint _required) Shareable(_owners, _required) {
+  }
+
+  function destroy() onlyManyOwners(sha3(msg.data)) {
+    selfdestruct(msg.sender);
+  }
+}
